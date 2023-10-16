7453af54185ee76bebe68baab7a189775fffda05
Mitroglou <nathanlgd14@gmail.com>
2023-10-16 19:21:12 +0200

    Ajout de la fonction login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\MarqueController;
 use App\Http\Controllers\ProduitController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\VenteController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,3 +23,14 @@ Route::resource('/vente', VenteController::class);
 Route::resource('/produit', ProduitController::class);
 Route::resource('/marque', MarqueController::class);
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
