88f55e26c441dc2c11c32f472eb5d0deda0e66ef
Mitroglou <nathanlgd14@gmail.com>
2023-10-17 00:03:34 +0200

    Ajout des langues

---
+++ app/Http/Middleware/LanguageMiddleware.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App;
+use Closure;
+use Config;
+use Session;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class LanguageMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // if (session::has('locale')) { Au choix
+        if (session('locale') !== null) {
+            App::setLocale(Session::get('locale'));
+        } else {
+            App::setLocale(Config::get('app.locale'));
+        }
+
+        return $next($request);
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,14 +11,14 @@
     <!-- Fonts -->
     {{-- <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" /> --}}
+
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
-
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body class="font-sans antialiased">
@@ -35,9 +35,9 @@
         @endif
 
         <!-- Page Content -->
-        <a href="{{ route('vente.index') }}">Liste des ventes</a>
-        <a href="{{ route('produit.index') }}">Liste des produits</a>
-        <a href="{{ route('marque.index') }}">Liste des marques</a>
+        <a class="btn btn-primary ms-3 mt-3" href="{{ route('vente.index') }}">Liste des ventes</a>
+        <a class="btn btn-primary ms-3 mt-3" href="{{ route('produit.index') }}">Liste des produits</a>
+        <a class="btn btn-primary ms-3 mt-3" href="{{ route('marque.index') }}">Liste des marques</a>
         @yield('content')
 
     </div>
